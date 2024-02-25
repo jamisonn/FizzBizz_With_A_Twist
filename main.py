0c15890cf43243deedf92cb5cc4bb30372d09ada
Jamison Norwood <11932076+jamisonn@users.noreply.github.com>
2024-02-25 12:06:43 -0500

    Updated with Mp3 Files, but code is currently broken

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+import os
+
 from playsound import playsound
+FizzSound = os.path.dirname(__file__) + '\\Hi-Hat-Closed-Hit-A1-www.fesliyanstudios.com.mp30'
+BuzzSound = os.path.dirname(__file__) + '\\Sounds/Bass-Drum-Hit-Level-5a-www.fesliyanstudios.com.mp3'
+
 
 def FizzBuzzMethod(num):
-        if num % 3 ==0:
-            print("Fizz")
-        if num % 5 ==0:
-            print("Buzz")
-        if num % 5 and num % 3==0:
-            print("FizzBuzz")
-i = 1
-while i<100:
-    FizzBuzzMethod(i)
-    i+=1
+    if num % 3 == 0:
+        print("Fizz")
+        playsound(FizzSound)
+    if num % 5 == 0:
+        print("Buzz")
+        playsound(BuzzSound)
+    if num % 5 and num % 3 == 0:
+        print("FizzBuzz")
+        playsound(FizzSound)
+        playsound(BuzzSound)
 
+
+i = 1
+while i < 100:
+    FizzBuzzMethod(i)
+    i += 1
