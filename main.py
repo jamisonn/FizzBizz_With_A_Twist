d878c10a437217c140affc146eeafabbd06c69cc
Jamison Norwood <11932076+jamisonn@users.noreply.github.com>
2024-02-26 16:16:52 -0500

    Mp3 Files are working properly.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
-
 from playsound import playsound
-FizzSound = os.path.dirname(__file__) + '\\Hi-Hat-Closed-Hit-A1-www.fesliyanstudios.com.mp30'
-BuzzSound = os.path.dirname(__file__) + '\\Sounds/Bass-Drum-Hit-Level-5a-www.fesliyanstudios.com.mp3'
 
+FizzSound = os.path.dirname(__file__) + '\\Sounds/Hi-Hat-Closed-Hit-A1-www.fesliyanstudios.com.mp3'
+BuzzSound = os.path.dirname(__file__) + '\\Sounds/Bass-Drum-Hit-Level-5a-www.fesliyanstudios.com.mp3'
 
 def FizzBuzzMethod(num):
     if num % 3 == 0:
